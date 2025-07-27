11cb21c6c7ca6ba9ff2d16eab43072ebe2d104ad
takshtyagi <takshtyagi80@gmail.com>
2025-07-27 22:45:25 +0530

    Updated Home2.js with new layout and content

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,13 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Taksh Tyagi </span>
+            from <span className="purple"> New Delhi, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I love turning creative ideas into interactive, responsive, and user-friendly web experiences. 
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed Bachelors in computer applications (BCA) at Amity
+            Noida.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,21 +24,22 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Listening music
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Watching sports
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Taksh</footer>
         </blockquote>
       </Card.Body>
     </Card>
   );
 }
 
-export default AboutCard;
+export default AboutCard; 
+
