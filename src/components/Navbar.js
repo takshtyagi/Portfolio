56378c8a41b6e6a5734dbddcdf508d7e60a04935
takshtyagi <takshtyagi80@gmail.com>
2025-07-29 22:39:50 +0530

    Updated Navbar.js with new layout and content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,9 @@ import React, { useState, useEffect } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork, CgFileDocument } from "react-icons/cg";
-import {
-  AiFillStar,
-  AiOutlineHome,
-  AiOutlineUser,
-} from "react-icons/ai";
+import { CgFileDocument } from "react-icons/cg";
+import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -33,9 +27,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand>
+        {/* Logo removed */}
 
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
@@ -74,16 +66,7 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/soumyajit4419/Portfolio"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
+            {/* Fork button removed */}
           </Nav>
         </Navbar.Collapse>
       </Container>
