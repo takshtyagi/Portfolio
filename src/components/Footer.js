088b2947ea5a5b209ad9c78f00a56fe5b2d05f8e
takshtyagi <takshtyagi80@gmail.com>
2025-08-01 22:53:13 +0530

    Updated Footer.js with new layout and content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Taksh Tyagi</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} TT</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
